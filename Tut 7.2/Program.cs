0dbad47765a4f86028e5fa61d5ce8b92d5ff534a
Larissa Trani <ltrani@utas.edu.au>
2022-04-12 14:48:25 +1000

    Add project files.

---
+++ Tut 7.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tut_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
